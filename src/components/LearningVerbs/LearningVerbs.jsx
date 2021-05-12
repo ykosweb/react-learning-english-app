dec0627476e806f1db5ed9174aa184611778eeda
Konstantin Yarmak <ykos.web@gmail.com>
2021-05-13 01:12:11 +0300

    add RightSide

--- src/components/LearningVerbs/LearningVerbs.jsx
+++ src/components/LearningVerbs/LearningVerbs.jsx
@@ -1,152 +1,66 @@
-import React, {useState} from 'react';
-import Input from "../UI/Input/Input";
-import './LearningVerbs.sass'
-import Icon from "../UI/Icon/Icon";
-import {faCheckCircle, faExclamationTriangle} from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
+import { Formik, Form, Field } from 'formik';
+import "./LearningVerbs.sass"
+import * as Yup from 'yup';
 
-const LearningVerbs = (props) => {
-    const [inputsData, setInputsData] = useState([
-        {
-            inputNumber: 0,
-            value: '',
-            type: 'text',
-            label: 'infinitive',
-            valid: false,
-            touched: false,
-            errorMessage: '',
-            validation: {
-                required: true,
-                minLength: 2
-            }
-        },
-        {
-            inputNumber: 1,
-            value: '',
-            type: 'text',
-            label: 'pastIndefinite',
-            valid: false,
-            touched: false,
-            errorMessage: '',
-            validation: {
-                required: true,
-                minLength: 2
-            }
-        },
-        {
-            inputNumber: 2,
-            value: '',
-            type: 'text',
-            label: 'pastParticiple',
-            valid: false,
-            touched: false,
-            errorMessage: '',
-            validation: {
-                required: true,
-                minLength: 2
-            }
-        }
-    ]);
-    const [validationState, setValidationState] = React.useState({
-        infinitive: false,
-        pastIndefinite: false,
-        pastParticiple: false
-    })
-    const inputState = {
-        success: 'check',
-        error: 'times'
-    }
+const SignupSchema = Yup.object().shape({
+    infinitive: Yup.string()
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Required'),
+    past: Yup.string()
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Required'),
+    pastParticiple: Yup.string().email('Invalid email').required('Required'),
+});
 
-    const validateInput = (currentInput, validation) => {
-        let {value, inputNumber} = currentInput;
-        if (value.trim() === props.verbItem.forms[inputNumber]) {
-            setValidationState({...validationState, validationState})
-        }
-    }
+const LearningVerbs = () => (
+    <div className="learningVerbsPage">
+        <div className="learningVerbsPage_Wrapper">
+            <h1 className="learningVerbsPage_Title">Signup</h1>
+            <Formik
+                initialValues={{
+                    infinitive: '',
+                    past: '',
+                    pastParticiple: '',
+                }}
+                validationSchema={SignupSchema}
+                onSubmit={values => {
+                    // same shape as initial values
+                    console.log(values);
+                }}
+            >
+                {({ errors, touched }) => (
+                    <Form className="learningVerbsPage_Form">
+                        <div className="learningVerbsPage_InputBlock">
+                            <label htmlFor="infinitive">Infinitive:</label>
+                            <Field name="infinitive" />
+                            {errors.infinitive && touched.infinitive ? (
+                                <div>{errors.infinitive}</div>
+                            ) : null}
+                        </div>
+                        <div className="learningVerbsPage_InputBlock">
+                            <label htmlFor="past">Past:</label>
+                            <Field name="past" />
+                            {errors.past && touched.past ? (
+                                <div>{errors.past}</div>
+                            ) : null}
+                        </div>
+                        <div className="learningVerbsPage_InputBlock">
+                            <label htmlFor="pastParticiple">Past Participle:</label>
+                            <Field name="pastParticiple"/>
+                            {errors.pastParticiple && touched.pastParticiple ? <div>{errors.pastParticiple}</div> : null}
+                        </div>
+                        <button type="submit">Submit</button>
 
-    const onChangeHandler = (event, inputItem) => {
-
-        const newInputsData = [...inputsData];
-        const currentInput = {...newInputsData[inputItem.inputNumber]};
-        const rightWord = props.verbItem.forms[inputItem.inputNumber];
-
-        currentInput.value = event.target.value;
-        currentInput.touched = true;
-        if (rightWord === currentInput.value.trim().toLowerCase()) {
-            currentInput.valid = true;
-            currentInput.errorMessage = '';
-        }
-        newInputsData[inputItem.inputNumber] = currentInput;
-        validateInput(currentInput, currentInput.validation)
-        setInputsData(newInputsData);
-    };
-    const onBlurHandler = (event, inputItem) => {
-        const rightWord = props.verbItem.forms[inputItem.inputNumber];
-        const newInputsData = [...inputsData];
-        const currentInput = {...newInputsData[inputItem.inputNumber]};
-        if (!currentInput.validation) {
-            currentInput.valid = true;
-        }
-        if (currentInput.value.length < inputItem.validation.minLength) {
-            currentInput.valid = false;
-            currentInput.errorMessage = 'Слишком короткое значение.';
-            return;
-        }
-        if (currentInput.value.trim().toLowerCase() !== rightWord) {
-            currentInput.errorMessage = 'Не верно, попробуйте другой вариант'
-        }
-        newInputsData[inputItem.inputNumber] = currentInput;
-
-        setInputsData(newInputsData)
-    }
-
-    const renderInputs = () => {
-        return inputsData.map((inputItem, index) => {
-            return (
-                <div className={"verbInputBlock"} key={index + inputItem.label}>
-                    <Input
-                        disabled={inputItem.valid}
-                        inputNuber={inputItem.inputNumber}
-                        type={inputItem.type}
-                        value={inputItem.value}
-                        valid={inputItem.valid}
-                        touched={inputItem.touched}
-                        label={inputItem.label + ':'}
-                        errorMessage={inputItem.errorMessage}
-                        shouldValidate={!!inputItem.validation}
-                        onChange={event => onChangeHandler(event, inputItem)}
-                        onBlur={event => onBlurHandler(event, inputItem)}
-                    />
-                    {inputItem.valid
-                        ? <Icon icon={faCheckCircle} color={"#2bc712"} size={1.5}/>
-                        : null
-                    }
-                    {inputItem.errorMessage
-                        ? <Icon icon={faExclamationTriangle} color={"red"} size={1.5}/>
-                        : null
-                    }
-                    {inputItem.errorMessage
-                        ? <div className={"errorMessage"}> {inputItem.errorMessage} </div>
-                        : null
-                    }
-                </div>
-            )
-        })
-    }
-
-
-    return (
-        <div className='learningVerbs'>
-            <div>
-                <p>Глагол - <strong className='activeWord'>{props.verbItem.verb}</strong></p>
-            </div>
-            <form className='verbsForm'>
-                {renderInputs()}
-                <button disabled={inputsData[0].valid || inputsData[1].valid || inputsData[2].valid === false}
-                        className="btn btn-success">Следующий глагол</button>
-            </form>
+                    </Form>
+                )}
+            </Formik>
         </div>
 
-    )
-}
+
+    </div>
+);
 
 export default LearningVerbs;
